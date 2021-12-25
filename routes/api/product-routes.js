19c68cee732da6d7aa636eabd2a56c7d0fa80d92
BrandonConte <bconte950@gmail.com>
2021-12-25 01:03:13 -0500

    Fixed tag for get product, tested routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
   Product.findAll({
     attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
     include: [
@@ -16,7 +17,9 @@ router.get('/', (req, res) => {
       },
       {
         model: Tag,
-        attributes: ['id', 'tag_name']
+        attributes: ['id', 'tag_name'],
+        through: Product,
+        as: 'product_tags'
       }
     ]
   })
